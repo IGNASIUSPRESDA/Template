a68f8372e33b6f53ed97b0f0483adafade2449b9
IGNASIUSPRESDA <presdasius@gmail.com>
2023-10-19 13:18:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const app = express();
 
-const server = http.createServer((req, res) => {
-    // Baca file HTML
-    fs.readFile('indexx.html', (err, data) => {
-        if (err) {
-            // Tangani kesalahan jika file tidak dapat dibaca
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Internal Server Error');
-        } else {
-            // Kirim file HTML sebagai respons
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(data);
-        }
-    });
+// Menentukan folder yang berisi file HTML
+const htmlFolder = __dirname;
+
+app.get('/', (req, res) => {
+    // Mengirimkan file HTML sebagai respons
+    res.sendFile(`${htmlFolder}/indexx.html`);
 });
 
 const port = 3000;
 
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server berjalan di http://localhost:${port}/`);
 });
